0e3d92411d409f79590628c5467457c58cb60864
tidanti <tipanovdaniil@gmail.com>
2022-01-28 01:36:09 +0300

    ex2 is done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, numberOfRepeats) {
+    let newStr = '';
+    if (numberOfRepeats < 0) {
+        newStr = 'ERROR';
+    }
 
+    for (let i = 0; i < numberOfRepeats; i++) {
+        newStr += str;
+    }
+
+    return newStr;
 };
 
 // Do not edit below this line
