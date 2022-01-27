ad7e36b887cab0c3343a12a09a56b22b09b15c08
tidanti <tipanovdaniil@gmail.com>
2022-01-28 01:42:31 +0300

    ex3 is done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let strSplittedArr = str.split('');
+    strSplittedArr = strSplittedArr.reverse();
+    let newStr = strSplittedArr.join('');
 
+    return newStr;
 };
 
 // Do not edit below this line
