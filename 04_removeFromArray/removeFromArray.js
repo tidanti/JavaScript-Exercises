2182278a2f81de7f07ce723b2aac558334cfa13d
tidanti <tipanovdaniil@gmail.com>
2022-01-29 17:49:35 +0300

    ex4 is done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elemsToDelete) {
+    const newArr = arr.filter((elem) => {
+        return !(elemsToDelete.includes(elem));
+    });
 
+    return newArr;
 };
 
 // Do not edit below this line
