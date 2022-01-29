db49ed2173983321aae0448d764474d6dfba604b
tidanti <tipanovdaniil@gmail.com>
2022-01-29 22:42:56 +0300

    ex7 is done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function(tempInF) {
+  let tempInC = (tempInF - 32) * 5 / 9;
+  tempInC = +tempInC.toFixed(1);
 
+  return tempInC;
 };
 
-const ctof = function() {
+const ctof = function(tempInC) {
+  let tempInF = tempInC * 9 / 5 + 32;
+  tempInF = +tempInF.toFixed(1);
 
+  return tempInF;
 };
 
 // Do not edit below this line
