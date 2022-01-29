313df35bef0666dc010fa7ce8ef8eab30317b720
tidanti <tipanovdaniil@gmail.com>
2022-01-29 20:29:39 +0300

    ex5 is done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (typeof min !== 'number' ||
+        typeof max !== 'number') {
+            return 'ERROR';
+    }
 
+    if (min < 0 || max < 0) return 'ERROR';
+
+    if (min > max) {
+        let tmp = min;
+        min = max;
+        max = tmp;
+    }
+
+    let sumAll = 0;
+    for (let i = min; i <= max; i++) {
+        sumAll += i;
+    }
+
+    return sumAll;
 };
 
 // Do not edit below this line
