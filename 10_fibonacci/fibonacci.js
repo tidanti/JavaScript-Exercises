837d4d9154b9859a5513c2d6e8aa6682a191e85c
Даниил Типанов <@expertanalitika.ru>
2022-02-10 14:44:10 +0300

    ex.10 is done WITHOUT TESTS!

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,39 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = +num;
+    const error = getErrorsOfInputValue(num);
+    if (error) return error;
 
+    const fibo = new Fibo(num);
+    return fibo.getNumberByIndex(num);
 };
+
+function getErrorsOfInputValue(value) {
+    if (!value) {
+        return 'OOPS!';
+    }
+
+    if (value <= 0) {
+        return 'Enter a positive value > 0!';
+    }
+
+    return '';
+}
+
+function Fibo(num) {
+    for (let i = 1; i <= num; i++) {
+        if (i === 1) {
+            this[i] = 1;
+            continue;
+        } else if (i === 2) {
+            this[i] = 1;
+            continue;
+        }
+
+        this[i] = this[i - 1] + this[i - 2];
+    }
+
+    this.getNumberByIndex = choicedNum => this[choicedNum];
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
