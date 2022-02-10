5a96d09e89f07f52fa8438c651cb8376a5cd63d0
Даниил Типанов <@expertanalitika.ru>
2022-02-10 10:04:19 +0300

    ex.8 is done WITHOUT TESTS!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+  if (numArray.length === 0) {
+    return 0;
+  }
+
+	return numArray.reduce((total, num) => {
+    return total += num;
+  }, 0);
 };
 
-const multiply = function() {
+const multiply = function(...numbers) {
+  if (numbers.length === 0) {
+    return 0;
+  }
 
+  return numbers.reduce((total, num) => {
+    return total *= num;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return Math.pow(num, power); // ?? rewrite?
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1;
+  }
+
+  let numFactorial = 1;
+  for (let i = num; num > 1; num--) {
+    numFactorial *= num;
+  }
+
+  return numFactorial;
 };
 
 // Do not edit below this line
