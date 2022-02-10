ed9572e26cd5d02faab7286c7e5c99352e4812a7
Даниил Типанов <@expertanalitika.ru>
2022-02-10 10:51:25 +0300

    ex.9 is done WITHOUT TESTS!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase;
+    const charArray = str.split();
+    const filteredCharArray = charArray.filter(currChar => currChar !== ',' && currChar !== ' ');
+    const reversedArray = filteredCharArray.reverse();
+    
+    const filteredOriginStr = filteredCharArray.join('');
+    const reversedStr = reversedArray.join('');
 
+    return filteredOriginStr === reversedStr;
 };
 
 // Do not edit below this line
