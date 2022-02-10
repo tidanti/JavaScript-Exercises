6ab311a69dac64dcb186bed9bc8d21d512a341e2
Даниил Типанов <@expertanalitika.ru>
2022-02-10 17:52:06 +0300

    ex.12 is done WITHOUT TESTS!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const orderedPeopleByAge = people.sort((a, b) => {
+        currentYear = (new Date()).getFullYear();
+        const aAge =
+                (a.yearOfDeath ? a.yearOfDeath : currentYear) - a.yearOfBirth;
+        const bAge =
+                (b.yearOfDeath ? b.yearOfDeath : currentYear) - b.yearOfBirth;
 
+        return (aAge > bAge) ? 1 : -1;
+    });
+
+    return orderedPeopleByAge[0];
 };
 
 // Do not edit below this line
