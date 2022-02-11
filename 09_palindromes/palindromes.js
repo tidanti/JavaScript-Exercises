33d353f3b5fe3fbb4a8dedc4ce2b3b21fa2d8bc1
tidanti <tipanovdaniil@gmail.com>
2022-02-11 19:57:47 +0300

    ex.9 is done with tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,11 +1,12 @@
 const palindromes = function (str) {
-    str = str.toLowerCase;
-    const charArray = str.split();
-    const filteredCharArray = charArray.filter(currChar => currChar !== ',' && currChar !== ' ');
-    const reversedArray = filteredCharArray.reverse();
+    str = str.toLowerCase();
+    const charArray = str.split('');
+    const filteredCharArray = charArray.filter(currChar => {
+        return currChar <= 'z' && currChar >= 'A';
+    });
     
     const filteredOriginStr = filteredCharArray.join('');
-    const reversedStr = reversedArray.join('');
+    const reversedStr = filteredCharArray.reverse().join('');
 
     return filteredOriginStr === reversedStr;
 };
