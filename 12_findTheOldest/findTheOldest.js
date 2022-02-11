026e142bc0b6344519d405fa224af60630b77db4
tidanti <tipanovdaniil@gmail.com>
2022-02-11 20:27:26 +0300

    ex.12 is done with tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -6,10 +6,12 @@ const findTheOldest = function(people) {
         const bAge =
                 (b.yearOfDeath ? b.yearOfDeath : currentYear) - b.yearOfBirth;
 
-        return (aAge > bAge) ? 1 : -1;
+        return (aAge > bAge) ? -1 : 1;
     });
 
     return orderedPeopleByAge[0];
+
+    // we can also resolve it with .reduce() method
 };
 
 // Do not edit below this line
